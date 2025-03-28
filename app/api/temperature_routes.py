a24005a5cb160db71aac29ff1bb5bf118ce8cdd4
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-03-28 14:38:46 +0200

    solution4

--- app/api/temperature_routes.py
+++ app/api/temperature_routes.py
@@ -1,15 +1,28 @@
-# app/api/temperature_routes.py
-from select import select
-
 import httpx
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_db
+from app import crud
 from app.models import Temperature
 from app.schemas import TemperatureOut
-from app import crud
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("WEATHER_API_KEY")
 
 router = APIRouter()
+
+
+async def get_temperature(city_name: str):
+    url = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city_name}"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        data = response.json()
+        temperature = data["current"]["temp_c"]
+        return temperature
+
 
 @router.get("/temperatures", response_model=list[TemperatureOut])
 async def get_temperatures(db: AsyncSession = Depends(get_db)):
@@ -17,19 +30,22 @@ async def get_temperatures(db: AsyncSession = Depends(get_db)):
     temperatures = result.scalars().all()
     return temperatures
 
+
 @router.get("/temperatures/{city_id}", response_model=list[TemperatureOut])
 async def get_temperatures_by_city(city_id: int, db: AsyncSession = Depends(get_db)):
     result = await db.execute(select(Temperature).filter(Temperature.city_id == city_id))
     temperatures = result.scalars().all()
     return temperatures
 
+
 @router.post("/temperatures/update")
 async def update_temperatures(db: AsyncSession = Depends(get_db)):
-    async with httpx.AsyncClient() as client:
-        cities = await crud.get_cities(db)
-        for city in cities:
-            response = await client.get(f"https://api.weatherapi.com/v1/current.json?key=YOUR_API_KEY&q={city.name}")
-            data = response.json()
-            temperature = data["current"]["temp_c"]
-            await crud.create_temperature(db, city.id, temperature)
+    cities = await crud.get_cities(db)
+
+    for city in cities:
+        temperature = await get_temperature(city.name)
+
+        await crud.create_temperature(db, city.id, temperature)
+
     return {"message": "Temperatures updated"}
+
